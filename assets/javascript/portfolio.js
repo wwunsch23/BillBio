6b9628b5fd92e215e237d779af19252686f9756a
Bill W <42258241+wwunsch23@users.noreply.github.com>
2018-10-31 18:13:12 -0500

    Updates to profile site

---
+++ assets/javascript/portfolio.js
@@ -0,0 +1,18 @@
+// Materialize - Initializers
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+    $('.sidenav').sidenav();
+    $('.tooltipped').tooltip();
+    $('.tabs').tabs();
+
+    // // Initialize collapse button
+    // $(".button-collapse").sideNav({
+    //     menuWidth: 190, // Default is 240
+    //     edge: 'left', // Choose the horizontal origin
+    //     closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    //     }
+    // );
+
+
+
+});
